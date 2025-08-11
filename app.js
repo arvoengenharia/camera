5a691efd5033db6e7b21c5f5e69c0d7a6a1868ef
arvoengenharia <felipenascimento@arvoengenharia.com.br>
2025-08-11 18:38:17 -0300

    deleteapp.js

--- app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then(reg => console.log('Service Worker registrado:', reg.scope))
-            .catch(err => console.error('Erro ao registrar o Service Worker:', err));
-    });
-}
